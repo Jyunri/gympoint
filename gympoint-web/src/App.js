16cedffbff7966956e840221c4963b234366c03a
Jonathan Suenaga <nekojimy@gmail.com>
2019-12-23 19:38:31 -0300

    add toast

--- gympoint-web/src/App.js
+++ gympoint-web/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-
+import { ToastContainer } from 'react-toastify';
 import './config/ReactotronConfig';
 
 import Routes from './routes';
@@ -19,6 +19,7 @@ function App() {
         <Router history={history}>
           <Routes />
           <GlobalStyle />
+          <ToastContainer autoClose={3000} />
         </Router>
       </PersistGate>
     </Provider>
--- gympoint-web/src/store/modules/auth/sagas.js
+++ gympoint-web/src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import api from '~/services/api';
 import history from '~/services/history';
 import { signInSuccess, signFailure } from './actions';
@@ -18,9 +19,9 @@ export function* signIn({ payload }) {
 
     history.push('/students');
   } catch (err) {
+    toast.error('Falha na autenticacao');
     yield put(signFailure());
   }
-
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
